9adc38b73af70777b9e080a6777783ea970360f3
VedVid <toalak@gmail.com>
2017-06-27 14:48:19 +0200

    add printPlayer before first blt screen refresh

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 /*
 Copyright (c) 2017 Tomasz "VedVid" Nowakowski ( v.v.roguelike@gmail.com )
-
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
 arising from the use of this software.
-
 Permission is granted to anyone to use this software for any purpose,
 including commercial applications, and to alter it and redistribute it
 freely, subject to the following restrictions:
-
 1. The origin of this software must not be misrepresented; you must not
    claim that you wrote the original software. If you use this software
    in a product, an acknowledgment in the product documentation would be
@@ -64,6 +61,7 @@ func printPlayer() {
 func loopOver() {
 	/*Function loopOver is main loop of the game.*/
 	for {
+		printPlayer()
 		blt.Refresh()
 		key := blt.Read()
 		if key == blt.TK_CLOSE || key == blt.TK_ESCAPE {
